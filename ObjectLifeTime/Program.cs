20fc0de6a3368bdea1e47963a66518f0039deffc
manel talbi <talbimanel20@gmail.com>
2022-01-08 13:35:59 +0100

    methods with the same name and differents arguments

--- ObjectLifeTime/Program.cs
+++ ObjectLifeTime/Program.cs
@@ -5,10 +5,14 @@
         static void Main(string[] args)
         {
             Car myCar = new Car();
+            /*
             myCar.Make = "oldmobile";
             myCar.Model = "cutlas Supreme";
             myCar.Year = 1986;
             myCar.Color = "silver";
+            */
+
+           //Car myThirdCar = new Car("Ford", "Escape",2005 , "White");
 
             Car myOtherCar;
             myOtherCar = myCar;
@@ -23,6 +27,15 @@
                 myCar.Model,
                 myCar.Year,
                 myCar.Color);
+            myOtherCar = null;
+            /*
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            */
+            myCar = null;
             Console.ReadLine();
 
         }
@@ -34,7 +47,20 @@
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
-        
-
+    /*
+        public Car()
+        {
+            // you could load from a configuration file,
+            // a database, etc.
+            Make = "Nissan";
+        }
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+       */ 
     }
 }
