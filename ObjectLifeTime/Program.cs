e4d6bfa74a8e6ad4b7d4747809d645122d05217b
manel talbi <talbimanel20@gmail.com>
2022-01-08 12:46:16 +0100

    two refferences to the same objesct

--- ObjectLifeTime/Program.cs
+++ ObjectLifeTime/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace simpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "oldmobile";
+            myCar.Model = "cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "silver";
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            myOtherCar.Model = "98";
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+            Console.ReadLine();
+
+        }
+       
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
+
+    }
+}
